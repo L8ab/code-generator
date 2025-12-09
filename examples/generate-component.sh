d35d99a513afdde63a11783729589b99d2c9f834
L8ab <gpf102030@gmail.com>
2025-12-10 01:40:22 +0300

    Add example: examples/generate-component.sh

---
+++ examples/generate-component.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+# Code Generator Usage Examples
+
+# Generate React component
+codegen react-component UserProfile src/components/UserProfile.jsx
+
+# Generate Express route
+codegen express-route products src/routes/products.js
+
+# Generate MongoDB model
+codegen mongodb-model Order src/models/Order.js
